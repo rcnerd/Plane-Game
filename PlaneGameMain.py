d72d21f1eda2f8bdded9da12f0ffe06db84f5f0a
rcnerd <hunter.g.wells@gmail.com>
2016-03-23 12:39:00 -0400

    Has a more proper execution.

--- PlaneGameMain.py
+++ PlaneGameMain.py
@@ -6,6 +6,7 @@ from Booster import *
 from Coin import *
 from Spike import *
 from Player import *
+from Level import *
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -32,4 +33,55 @@ all = pygame.sprite.OrderedUpdates()
 
 level = Level("Levels\level1.layout")
 
+#player = level.player
+
 print "I exist"
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.go("up")
+            elif event.key == pygame.K_DOWN:
+                player.go("down")
+            elif event.key == pygame.K_LEFT:
+                player.go("left")
+            elif event.key == pygame.K_RIGHT:
+                player.go("right")
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player.go("stop up")
+            elif event.key == pygame.K_DOWN:
+                player.go("stop down")
+            elif event.key == pygame.K_LEFT:
+                player.go("stop left")
+            elif event.key == pygame.K_RIGHT:
+                player.go("stop right")
+    
+    all.update(size)
+    
+    #playersHitBalls = pygame.sprite.groupcollide(players, balls, False, True)
+    #playersHitBoundries = pygame.sprite.groupcollide(players, boundries, False, False)
+    #ballsHitBoundries = pygame.sprite.groupcollide(balls, boundries, False, False)
+    #ballsHitBalls = pygame.sprite.groupcollide(balls, balls, False, False)
+    
+    #for p in playersHitBoundries:
+        #for boundry in playersHitBoundries[p]:
+            #p.collideBoundry(boundry)
+    
+    #for ball in ballsHitBoundries:
+        #for boundry in ballsHitBoundries[ball]:
+            #ball.collideBoundry(boundry)
+    
+    #for ball1 in ballsHitBalls:
+        #for ball2 in ballsHitBalls[ball1]:
+            #ball1.collideBall(ball2)
+    
+    bgColor = r,g,b
+    screen.fill(bgColor)
+    dirty = all.draw(screen)
+    pygame.display.update(dirty)
+    pygame.display.flip()
+    clock.tick(60)
