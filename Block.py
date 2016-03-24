213e6ba4faad814040af3eab24a752017c072107
rcnerd <hunter.g.wells@gmail.com>
2016-03-24 12:39:53 -0400

    fixed it need to rename Object Object

--- Block.py
+++ Block.py
@@ -1,5 +1,6 @@
 import sys, pygame, math, random
 from Object import Object
-class Block(pygame.sprite.Sprite):
+
+class Block(Object):
     def __init__(self, image, pos=[0,0]):
         Object.__init__(self, image, pos=[0,0])
